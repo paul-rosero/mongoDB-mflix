c4570fdd5f42cae6484dbbf82d2c2f33befe9792
Paul Rosero <paul.rosero23@gmail.com>
2020-12-03 20:11:46 -0500

    add lines 65-67 to find all movies and show only _id and title

--- src/dao/moviesDAO.js
+++ src/dao/moviesDAO.js
@@ -44,7 +44,6 @@ export default class MoviesDAO {
    * @returns {Promise<CountryResult>} A promise that will resolve to a list of CountryResults.
    */
   static async getMoviesByCountry(countries) {
-    console.log(countries)
     /**
     Ticket: Projection
 
@@ -62,12 +61,14 @@ export default class MoviesDAO {
       // and _id. Do not put a limit in your own implementation, the limit
       // here is only included to avoid sending 46000 documents down the
       // wire.
-      cursor = await movies.find().limit(1)
+      cursor = await movies.find(
+        {countries: {$in: countries }},
+        { projection: {title: 1}}
+      )  
     } catch (e) {
       console.error(`Unable to issue find command, ${e}`)
       return []
     }
-
     return cursor.toArray()
   }
 
