2b8d3861765c78fbe2063eda077131cf98eb01c4
Paul Rosero <paul.rosero23@gmail.com>
2020-12-13 16:58:22 -0500

    use the variables to insert the info from those variables

--- src/dao/usersDAO.js
+++ src/dao/usersDAO.js
@@ -61,7 +61,7 @@ export default class UsersDAO {
       let { name, email, password } = userInfo
       // TODO Ticket: Durable Writes
       // Use a more durable Write Concern for this operation.
-      await users.insertOne({ someField: "someValue" })
+      await users.insertOne({ name, email, password })
       return { success: true }
     } catch (e) {
       if (String(e).startsWith("MongoError: E11000 duplicate key error")) {
