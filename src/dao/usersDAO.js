b5bf05e639db31746f7273c65326bd01a75b0aa2
Paul Rosero <paul.rosero23@gmail.com>
2020-12-13 18:05:37 -0500

    no test implemented but decided to give it a writeConcern of majority

--- src/dao/usersDAO.js
+++ src/dao/usersDAO.js
@@ -61,7 +61,7 @@ export default class UsersDAO {
       let { name, email, password } = userInfo
       // TODO Ticket: Durable Writes
       // Use a more durable Write Concern for this operation.
-      await users.insertOne({ name, email, password })
+      await users.insertOne({ name, email, password }, {w: "majority"})
       return { success: true }
     } catch (e) {
       if (String(e).startsWith("MongoError: E11000 duplicate key error")) {
