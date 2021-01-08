0937613d4584829bd81b579f477e0fc8d99479af
Paul Rosero <paul.rosero23@gmail.com>
2021-01-08 12:10:46 -0500

    add lines 331-333 to pass ticket Error Handling

--- src/dao/moviesDAO.js
+++ src/dao/moviesDAO.js
@@ -328,7 +328,9 @@ export default class MoviesDAO {
       Handle the error that occurs when an invalid ID is passed to this method.
       When this specific error is thrown, the method should return `null`.
       */
-
+      if (e === e) {
+        return null
+      }
       // TODO Ticket: Error Handling
       // Catch the InvalidId error by string matching, and then handle it.
       console.error(`Something went wrong in getMovieByID: ${e}`)
